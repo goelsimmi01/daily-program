0fd10ddb78ca0f07b174d4c53ebe9699da663799
goelsimmi01 <117914166+goelsimmi01@users.noreply.github.com>
2022-11-17 17:01:22 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Addition
+{
+public static void main(String[]args)
+{
+System.out.println("Addition");
+Scanner s = new Scanner(System.in);
+System.out.println("Enter the num1");
+int a = s.nextInt();
+System.out.println("Enter the num2");
+int b = s.nextInt();
+int sum = a+b;
+System.out.println("Sum of numbers is ="+sum );
+}
+
+
+
+
+
+
+
+
+
+} 
