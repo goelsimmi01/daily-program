b75a067c4335198351c0673325e992d99110195a
goelsimmi01 <117914166+goelsimmi01@users.noreply.github.com>
2022-11-17 17:13:01 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,24 @@
+class MinMax
+{
+
+public static void main(String[]args)
+{
+System.out.println("Integer.MIN_VALUE="+Integer.MIN_VALUE);
+System.out.println("Integer.MAX_VALUE="+Integer.MAX_VALUE);
+System.out.println("Integer.SIZE="+Integer.SIZE);
+
+System.out.println("float.MIN_VALUE="+Float.MIN_VALUE);
+System.out.println("float.MAX_VALUE="+Float.MAX_VALUE);
+System.out.println("float.SIZE="+Float.SIZE);
+
+System.out.println("byte.MIN_VALUE="+Byte.MIN_VALUE);
+System.out.println("byte.MAX_VALUE="+Byte.MAX_VALUE);
+System.out.println("byte.SIZE="+Byte.SIZE);
+
+System.out.println("short.MIN_VALUE="+Short.MIN_VALUE);
+System.out.println("short.MAX_VALUE="+Short.MAX_VALUE);
+System.out.println("short.SIZE="+Short.SIZE);
+
+}
+
+}
